3f888dbec28f0636fcc253895155ac61ea39a8e9
Tatsiujin Chin <c910335@gmail.com>
2019-12-06 20:29:27 +0800

    improve as8 hash table

--- as8/main.c
+++ as8/main.c
@@ -3,7 +3,7 @@
 #define ht_size 15485867
 
 typedef struct e {
-  int n, i, j;
+  int n;
   struct e *t;
 } e;
 
@@ -11,20 +11,15 @@ int n, a[2500], i, j;
 e *h[ht_size], *t;
 
 int check(int n) {
-  t = h[(n % ht_size + ht_size) % ht_size];
-  while (t != NULL) {
-    if (t->n == n && i != t->i && i != t->j && j != t->j)
+  for (t = h[(n % ht_size + ht_size) % ht_size]; t; t = t->t)
+    if (t->n == n)
       return 1;
-    t = t->t;
-  }
   return 0;
 }
 
 void add(int n) {
   t = (e*) malloc(sizeof(e));
   t->n = n;
-  t->i = i;
-  t->j = j;
   t->t = h[(n % ht_size + ht_size) % ht_size];
   h[(n % ht_size + ht_size) % ht_size] = t;
 }
@@ -33,14 +28,16 @@ int main() {
   scanf("%d", &n);
   for (i = 0; i != n; ++i)
     scanf("%d", a + i);
-  for (i = 0; i != n; ++i)
-    for (j = i + 1; j < n; ++j) {
-      if (check(-a[i] - a[j])) {
+  for (i = 1; i < n - 2; ++i) {
+    for (j = 0; j < i; ++j)
+      add(a[i] + a[j]);
+    for (j = i + 2; j < n; ++j) {
+      if (check(-a[i + 1] - a[j])) {
         putchar('1');
         return 0;
       }
-      add(a[i] + a[j]);
     }
+  }
   putchar('0');
   return 0;
 }
